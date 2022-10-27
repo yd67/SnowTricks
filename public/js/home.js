5df28fccee28eb7a5f19e8a735cc8957eb021c51
Darius <yvens67@hotmail.fr>
2022-10-27 19:31:33 +0200

    ajout  "===" dans la condition

--- public/js/home.js
+++ public/js/home.js
@@ -39,13 +39,13 @@ function backToTop() {
 //  partie load more 
 $(document).ready(function () {
   $(".block").slice(0, 6).show();
-  if ($(".block:hidden").length != 0) {
+  if ($(".block:hidden").length !== 0) {
     $("#load").show();
   }
   $("#load").on("click", function (e) {
     e.preventDefault();
     $(".block:hidden").slice(0, 3).slideDown();
-    if ($(".block:hidden").length == 0) {
+    if ($(".block:hidden").length === 0) {
       $("#load").hide();
     }
   });
