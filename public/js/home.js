3aaedf766fa2c4f3a2100cbf2e329f4af21636cd
Darius Yvens <74614065+yd67@users.noreply.github.com>
2022-10-30 20:19:43 +0100

    Revert "Amelioration code"

--- public/js/home.js
+++ public/js/home.js
@@ -19,34 +19,34 @@ function scrollFunction() {
 }
 
 btnToTop.addEventListener("click", backToTop);
-btnToDown.addEventListener("click", goDown);
+btnToDown.addEventListener("click",goDown) ;
 
 function goDown() {
   document.documentElement.scrollTo({
-    top: 800,
-    left: 0,
-    behavior: "smooth"
+      top: 800,
+      left: 0,
+      behavior: "smooth"
   })
 }
 function backToTop() {
   document.documentElement.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: "smooth"
+      top: 0,
+      left: 0,
+      behavior: "smooth"
   })
 }
 
 //  partie load more 
-$(document).ready(function () {
+$(document).ready(function() {
   $(".block").slice(0, 6).show();
-  if ($(".block:hidden").length !== 0) {
-    $("#load").show();
+  if ($(".block:hidden").length != 0) {
+      $("#load").show();
   }
-  $("#load").on("click", function (e) {
-    e.preventDefault();
-    $(".block:hidden").slice(0, 3).slideDown();
-    if ($(".block:hidden").length === 0) {
-      $("#load").hide();
-    }
+  $("#load").on("click", function(e) {
+      e.preventDefault();
+      $(".block:hidden").slice(0, 3).slideDown();
+      if ($(".block:hidden").length == 0) {
+          $("#load").hide();
+      }
   });
 }) 
